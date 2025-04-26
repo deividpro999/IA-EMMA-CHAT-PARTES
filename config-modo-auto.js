4de7e4bc0e790ff580db790a9a8250fb20aba3e2
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-04-26 03:17:58 -0300

    Update config-modo-auto.js

--- config-modo-auto.js
+++ config-modo-auto.js
@@ -6,3 +6,14 @@ function toggleConfigMenu() {
 function trocarFonte(fonte) {
   document.body.style.fontFamily = fonte;
 }
+
+function trocarFundo(fundo) {
+  if (fundo.startsWith('url')) {
+    document.body.style.backgroundImage = fundo;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+  } else {
+    document.body.style.backgroundImage = "";
+    document.body.style.backgroundColor = fundo;
+  }
+}
