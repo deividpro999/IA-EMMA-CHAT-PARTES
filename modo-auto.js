3afd31c3537973aed765253033f2047037a4f515
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-04-27 15:50:39 -0300

    Update modo-auto.js

--- modo-auto.js
+++ modo-auto.js
@@ -2,7 +2,7 @@ const mensagens = [
   { hora: 6, texto: "Bom dia! Vamos fazer esse dia brilhar!", audio: "https://github.com/deividpro999/IA-EMMA-CHAT-PARTES/raw/07e3f6b6d1af2d935615b7cd5a9ad56e0dc89f7a/audios/Bom%20dia!%20Vamos%20fazer%20esse%20dia%20brilhar!.mp3" },
   { hora: 12, texto: "Boa tarde! Como você está se sentindo hoje?", audio: "https://github.com/deividpro999/IA-EMMA-CHAT-PARTES/raw/8fad33cb6b6e4e9d67364a11d4614743d910e1c6/audios/Boa%20tarde!%20Como%20voc%C3%AA%20est%C3%A1%20se%20sentindo%20hoje!.mp3" },
   { hora: 18, texto: "Boa noite! O que você conquistou hoje?", audio: "https://github.com/deividpro999/IA-EMMA-CHAT-PARTES/raw/980af71f31e640d7a4c37e60b71440c1f92cb3ed/audios/Boa%20noite!%20O%20que%20voc%CC%83e%20conquistou%20hoje!.mp3" },
-  { texto: "Nunca desista! Cada passo é uma vitória.", audio: "audios/motivacional1.mp3" },
+  { texto: "Nunca desista! Cada passo é uma vitória.", audio: "https://github.com/deividpro999/IA-EMMA-CHAT-PARTES/raw/980af71f31e640d7a4c37e60b71440c1f92cb3ed/audios/Nunca%20desista!%20Cada%20passo%20%C3%A9%20uma%20vit%C3%B3ria..mp3" },
   { texto: "Acredite em você! O melhor está por vir.", audio: "audios/motivacional2.mp3" },
   { texto: "Você é incrível! Continue assim.", audio: "audios/motivacional3.mp3" }
 ];
