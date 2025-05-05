ba4a6fd84cea7b9a54c0482c5822ce7549bfbb00
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-05-05 03:23:59 -0300

    Update opções.js

--- opções.js
+++ opções.js
@@ -1,11 +1,1 @@
-function navigateTo(option) {
-    switch(option) {
-        case 'diretor-animation-story':
-            carregarConteudo('diretor-animation-story.html');
-            break;
-        case 'opcoes':
-            carregarConteudo('opcoes.html');
-            break;
-        // outros cases se precisar
-    }
-}
+
