262f663707042d590a1712652298d2b515135bfa
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-05-05 03:12:00 -0300

    Create opções.js

---
+++ opções.js
@@ -0,0 +1,11 @@
+function navigateTo(option) {
+    switch(option) {
+        case 'diretor-animation-story':
+            carregarConteudo('diretor-animation-story.html');
+            break;
+        case 'opcoes':
+            carregarConteudo('opcoes.html');
+            break;
+        // outros cases se precisar
+    }
+}
